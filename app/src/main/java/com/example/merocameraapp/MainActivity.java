56ef91e895a8e32dab7113c2394765e2893cf4d7
Softwarica College Lab <softwarica@softwarica.com>
2019-04-26 08:35:49 +0545

    final commit

---
+++ app/src/main/java/com/example/merocameraapp/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.merocameraapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import javax.xml.transform.Result;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageview;
+    private Button button_image;
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageview = findViewById(R.id.imageview);
+        button_image = findViewById(R.id.button_image);
+
+       checkPermission();
+       button_image.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               loadCamera();
+           }
+
+           private void loadCamera() {
+               Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+               if (intent.resolveActivity(getPackageManager()) !=null){
+                   startActivityForResult(intent, 1);
+               }
+           }
+       });
+    }
+
+    private void checkPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==1 && requestCode == RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageview.setImageBitmap(imageBitmap);
+        }
+    }
+}
